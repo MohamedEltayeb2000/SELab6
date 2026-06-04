5d75187dd7fa14c947099afa3b8a764e67b16ffb
Mohamed Elaraby <mohamed2000eltayeb@gmail.com>
2026-06-04 01:02:11 -0500

    second coomit

--- src/main/java/g1/MyMain.java
+++ src/main/java/g1/MyMain.java
@@ -10,6 +10,7 @@ import java.util.Arrays;
 public class MyMain {
 
     public static void main(String[] args) {
+        System.out.println("hello world");
         PatientService patientService=new PatientService();
 
       Patient[] patients=patientService.sortPatientWithAge();
